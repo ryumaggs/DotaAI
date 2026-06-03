fbc3e42f3ab77c172a70758bfb816deb47698d46
ravensaye <ravensaye@gmail.com>
2026-06-03 15:46:32 -0400

    added load_api_key function

--- data/data_util.py
+++ data/data_util.py
@@ -1,4 +1,5 @@
 import requests
+from pathlib import Path
 def get(url, params=None, api_key=None):
     params = params or {}
     if api_key:
@@ -6,3 +7,9 @@ def get(url, params=None, api_key=None):
     r = requests.get(url, params=params)
     r.raise_for_status()
     return r.json()
+
+def load_api_key():
+    key_path = Path(__file__).parent.parent / 'secrets' / 'api_key.txt'
+    with open(key_path, 'r') as f:
+        key = f.read().strip()
+    return key
